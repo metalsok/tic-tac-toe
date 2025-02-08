6780dbca8321d0ae185d74343933061cac839ce2
Sokratis <sokratis.metallidis@agilox.net>
2025-02-08 17:49:40 +0100

    Added playername editing & gameboard structure

---
+++ src/components/Player.tsx
@@ -0,0 +1,23 @@
+import {useState} from "react";
+
+export default function Player({name, symbol}: { name: string, symbol: string }) {
+    const [isEditing, setIsEditing] = useState(false)
+    const [playerName, setPlayerName] = useState(name)
+
+    function toggleEditing() {
+        setIsEditing((prevState) => !prevState)
+    }
+
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setPlayerName(event.target.value)
+    }
+
+    return <li>
+                    <span className="player">
+                        {!isEditing && <span className="player-name">{playerName}</span>}
+                        {isEditing && <input type="text" required value={playerName} onChange={handleChange}/>}
+                        <span className="player-symbol">{symbol}</span>
+                    </span>
+        <button onClick={toggleEditing}>{!isEditing ? 'Edit' : 'Save'}</button>
+    </li>
+}
