439587ae440b7d9fd5ff01385afb4988b2051093
Sokratis <sokratis.metallidis@agilox.net>
2025-02-08 18:01:45 +0100

    Add board update on click

--- src/components/Gameboard.tsx
+++ src/components/Gameboard.tsx
@@ -1,11 +1,22 @@
+import {useState} from "react";
+
 const initialGameBoard = [[null, null, null], [null, null, null], [null, null, null]]
 export default function GameBoard() {
-    return <ol id="game-board">
-        {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
-            <ol>
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) => {
+            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
+            updatedBoard[rowIndex][colIndex] = 'X'
+            return updatedBoard
+        })
+    }
+
+    return <ol id="game-board">
+        {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
+            <ol>
                 {row.map((playerSymbol, colIndex) => <li key={colIndex}>
-                    <button>{playerSymbol}</button>
+                    <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                 </li>)}
             </ol>
         </li>)}
